0222c223859a5ca1800fb659254241f9132e68b7
Scott <scruwys@gmail.com>
2020-06-12 14:52:12 -0700

    build(dockerhub): Add version tracking

--- metamapper/__init__.py
+++ metamapper/__init__.py
@@ -3,4 +3,17 @@ from __future__ import absolute_import
 
 # This will make sure the app is always imported when
 # Django starts so that shared_task will use this app.
-from .celery import app
+import_failed = False
+
+try:
+    from .celery import app
+except ImportError:
+    try:
+        from django.conf import settings
+    except ImportError:
+        import_failed = True
+
+    if import_failed:
+        raise
+
+__version__ = '0.1.0'
