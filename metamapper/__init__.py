82b13aa68973b5b45f490a16ca302c5ffa5b868c
Scott <scruwys@gmail.com>
2020-06-12 15:32:23 -0700

    fix(import): Fix import during version checks

--- metamapper/__init__.py
+++ metamapper/__init__.py
@@ -3,17 +3,21 @@ from __future__ import absolute_import
 
 # This will make sure the app is always imported when
 # Django starts so that shared_task will use this app.
-import_failed = False
+raise_import_exception = False
 
 try:
     from .celery import app
 except ImportError:
+    raise_import_exception = True
+    # If we can't import Django settings, then we aren't trying
+    # to run the actual application. Which means we are just grabbing
+    # the version.
     try:
         from django.conf import settings
     except ImportError:
-        import_failed = True
+        raise_import_exception = False
 
-    if import_failed:
+    if raise_import_exception:
         raise
 
 __version__ = '0.1.0'
