1741543c2d4892e91e9215a591a55876c5a5cb31
Scott <scruwys@gmail.com>
2020-08-23 10:41:08 -0700

    fix(omnisearch): Apply datastore-level permissions to search results

--- app/omnisearch/backends/__init__.py
+++ app/omnisearch/backends/__init__.py
@@ -8,7 +8,7 @@ from django.conf import settings
 module_name, class_name = os.path.splitext(settings.SEARCH_BACKEND)
 
 
-def get_search_backend():
+def get_search_backend(workspace, user):
     """BaseSearchBackend: Get the current search backend.
     """
-    return getattr(importlib.import_module(module_name), class_name[1:])()
+    return getattr(importlib.import_module(module_name), class_name[1:])(workspace, user)
--- app/omnisearch/tests/test_queries.py
+++ app/omnisearch/tests/test_queries.py
@@ -3,6 +3,8 @@ import testutils.cases as cases
 import testutils.decorators as decorators
 import testutils.factories as factories
 
+from app.definitions.models import Datastore
+
 
 class TestOmnisearch(cases.GraphQLTestCase):
     """Test cases for listing all datastores in a workspace.
@@ -22,6 +24,7 @@ class TestOmnisearch(cases.GraphQLTestCase):
           pk
           modelName
           score
+          datastoreId
           searchResult {
             label
             description
@@ -36,22 +39,64 @@ class TestOmnisearch(cases.GraphQLTestCase):
     def setUp(self):
         super(TestOmnisearch, self).setUp()
 
+        self.datastore = Datastore.objects.get(id='s4N8p5g0wjiS')
         self.other_datastore = factories.DatastoreFactory(workspace=self.workspace)
         self.other_schema = factories.SchemaFactory(datastore=self.other_datastore)
         self.other_table = factories.TableFactory(schema=self.other_schema, name='customers')
 
-    @decorators.as_someone(['MEMBER', 'READONLY', 'OWNER'])
-    def test_query_returns_results_for_all_datastores(self):
-        """It should return results for all datastores in the workspace.
+    def execute_search_query(self):
+        """Used for consistent test results.
         """
         content = 'customer information'
         results = self.execute(self.statement, variables={'content': content})
         results = results['data'][self.operation]
+        return results
+
+    @decorators.as_someone(['MEMBER', 'READONLY', 'OWNER'])
+    def test_query_when_object_permissions_are_disabled(self):
+        """It should return results for all datastores in the workspace.
+        """
+        results = self.execute_search_query()
 
         self.assertTrue(len(results) > 1)
         self.assertTrue(isinstance(results['timeElapsed'], (float,)))
         self.assertTrue(str(self.other_table.pk) in [d['pk'] for d in results['searchResults']])
         self.assertEqual(set([d['modelName'] for d in results['searchResults']]), {'Column', 'Comment', 'Table'})
+        self.assertEqual(
+            set([d['datastoreId'] for d in results['searchResults']]),
+            {self.datastore.id, self.other_datastore.id},
+        )
+
+    @decorators.as_someone(['MEMBER', 'READONLY'])
+    def test_query_when_object_permissions_are_enabled_without_access(self):
+        """It filter out datastore results if does not have permissions.
+        """
+        self.other_datastore.object_permissions_enabled = True
+        self.other_datastore.save()
+
+        results = self.execute_search_query()
+
+        self.assertTrue(len(results) > 1)
+        self.assertEqual(
+            set([d['datastoreId'] for d in results['searchResults']]),
+            {self.datastore.id},
+        )
+
+    @decorators.as_someone(['MEMBER', 'READONLY'])
+    def test_query_when_object_permissions_are_enabled_with_access(self):
+        """It should honor object-level permissions when enabled.
+        """
+        self.other_datastore.object_permissions_enabled = True
+        self.other_datastore.save()
+        self.other_datastore.assign_perm(self.user, 'definitions.view_datastore')
+
+        results = self.execute_search_query()
+
+        self.assertTrue(len(results) > 1)
+        self.assertEqual(
+            set([d['datastoreId'] for d in results['searchResults']]),
+            {self.datastore.id, self.other_datastore.id},
+        )
 
     @decorators.as_someone(['MEMBER', 'READONLY', 'OWNER'])
     def test_query_returns_results_for_a_single_datastore(self):
@@ -65,6 +110,10 @@ class TestOmnisearch(cases.GraphQLTestCase):
         results = results['data'][self.operation]
 
         self.assertTrue(len(results['searchResults']) == 1)
+        self.assertEqual(
+            set([d['datastoreId'] for d in results['searchResults']]),
+            {self.other_datastore.id},
+        )
 
     @decorators.as_someone(['MEMBER', 'READONLY', 'OWNER'])
     def test_query_returns_results_for_only_this_workspace(self):
