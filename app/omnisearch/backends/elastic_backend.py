8e69e0bcf143bb80ef37ed3ec8d06cee78b44ad8
Conrad Nguyen <conradn86@gmail.com>
2020-09-20 17:58:17 -0500

    feat: Add back datastore_id filtering.

--- app/omnisearch/backends/elastic_backend.py
+++ app/omnisearch/backends/elastic_backend.py
@@ -49,11 +49,19 @@ class ElasticBackend(base.BaseSearchBackend):
 
         return self.workspace.id, [datastore.id for datastore in datastores]
 
-    def search(self, search_query_string, start=0, size=100, **extra_filters):
+    def search(self, search_query_string, datastore_id=None, start=0, size=100, **extra_filters):
         workspace_id, datastore_ids = self.user_permission_ids()
 
         if not datastore_ids:
+            # User has no access to any datastores.
             return []
+
+        if datastore_id and datastore_id not in datastore_ids:
+            # User is requesting a datastore they don't have access to.
+            return []
+
+        elif datastore_id:
+            datastore_ids = [datastore_id]
 
         s = Search(using=self.client)
         s = s.query(
