581d15b06cceae461b52e6d0ef1e0e4e6558b931
Scott <scruwys@gmail.com>
2020-10-15 10:15:30 -0400

    feat: Make exporting tables possible

--- www/src/lib/utilities.js
+++ www/src/lib/utilities.js
@@ -157,3 +157,15 @@ export const getWithExpiry = (key) => {
   }
   return item.value
 }
+
+export const generateGuid = () => {
+  var result, i, j;
+  result = '';
+  for(j=0; j<32; j++) {
+    if( j === 8 || j === 12 || j === 16 || j === 20)
+      result = result + '-';
+    i = Math.floor(Math.random()*16).toString(16).toUpperCase();
+    result = result + i;
+  }
+  return result;
+}
