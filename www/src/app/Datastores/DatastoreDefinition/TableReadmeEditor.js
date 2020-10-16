ead4dd0fe55c31c9ed44d6a1105d6989f576a775
Scott <scruwys@gmail.com>
2020-10-16 12:04:46 -0400

    reeadme

--- www/src/app/Datastores/DatastoreDefinition/TableReadmeEditor.js
+++ www/src/app/Datastores/DatastoreDefinition/TableReadmeEditor.js
@@ -1,10 +1,98 @@
 import React, { Component } from "react"
-import { Col, Row } from "antd"
+import PropTypes from "prop-types"
+const CodeMirror = window.CodeMirror
 
-class DefinitionReadmeEditor extends Component {
+const IS_MOBILE = typeof navigator === 'undefined' || (
+  navigator.userAgent.match(/Android/i)
+    || navigator.userAgent.match(/webOS/i)
+    || navigator.userAgent.match(/iPhone/i)
+    || navigator.userAgent.match(/iPad/i)
+    || navigator.userAgent.match(/iPod/i)
+    || navigator.userAgent.match(/BlackBerry/i)
+    || navigator.userAgent.match(/Windows Phone/i)
+)
+
+class CodeMirrorEditor extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {isControlled: Boolean(this.props.value)}
+    this.handleChange = this.handleChange.bind(this)
+    this.editorRef = React.createRef()
+  }
+
+  componentDidMount() {
+    const isTextArea = this.props.forceTextArea || IS_MOBILE;
+    if (!isTextArea) {
+      this.editor = CodeMirror.fromTextArea(this.editorRef.current, this.props);
+      this.editor.on('change', this.handleChange);
+    }
+  }
+
+  componentDidUpdate() {
+    if (!this.editor) {
+      return
+    }
+
+    if (this.props.value) {
+      if (this.editor.getValue() !== this.props.value) {
+        this.editor.setValue(this.props.value);
+      }
+    }
+  }
+
+  handleChange() {
+    if (!this.editor) {
+      return
+    }
+
+    const value = this.editor.getValue()
+    if (value === this.props.value) {
+      return
+    }
+
+    if (this.props.onChange) {
+      this.props.onChange({target: {value: value}})
+    }
+
+    if (this.editor.getValue() !== this.props.value) {
+      if (this.state.isControlled) {
+        this.editor.setValue(this.props.value)
+      } else {
+        this.props.value = value
+      }
+    }
+  }
+
   render() {
-    return <p>Test</p>
+    const editor = React.createElement('textarea', {
+      ref: this.editorRef,
+      value: this.props.value,
+      readOnly: this.props.readOnly,
+      defaultValue: this.props.defaultValue,
+      onChange: this.props.onChange,
+      className: this.props.textAreaClassName
+    })
+
+    return React.createElement('div', null, editor);
   }
 }
 
+const DefinitionReadmeEditor = (props) => {
+  return (
+    <form className="editor">
+      <CodeMirrorEditor mode="markdown" theme="solarized" value={props.value} onChange={props.onChange} />
+    </form>
+  )
+}
+
+DefinitionReadmeEditor.propTypes = {
+  onChange: PropTypes.func.isRequired,
+  value: PropTypes.string
+}
+
+DefinitionReadmeEditor.defaultProps = {
+  value: ''
+}
+
 export default DefinitionReadmeEditor
+
--- www/src/pages/DatastoreDefinition/DefinitionReadmeEditor.js
+++ www/src/pages/DatastoreDefinition/DefinitionReadmeEditor.js
@@ -1,19 +1,132 @@
 import React, { Component } from "react"
-import { Col, Row } from "antd"
+import { Button, Icon } from "antd"
+import PropTypes from "prop-types"
 import TableReadmeEditor from "app/Datastores/DatastoreDefinition/TableReadmeEditor"
-import TableReadmePreview from "app/Datastores/DatastoreDefinition/TableReadmePreview"
+import Markdown from "react-markdown/with-html"
 
-class DefinitionReadmeEditor extends Component {
+const hljs = window.hljs
+
+
+const initialSource = `
+# Live demo
+Changes are automatically rendered as you type.
+## Table of Contents
+* Implements [GitHub Flavored Markdown](https://github.github.com/gfm/)
+* Renders actual, "native" React DOM elements
+* Allows you to escape or skip HTML (try toggling the checkboxes above)
+* If you escape or skip the HTML, no \`dangerouslySetInnerHTML\` is used! Yay!
+## HTML block below
+<blockquote>
+  This blockquote will change based on the HTML settings above.
+</blockquote>
+## How about some code?
+\`\`\`js
+var React = require('react');
+var Markdown = require('react-markdown');
+React.render(
+  <Markdown source="# Your markdown here" />,
+  document.getElementById('content')
+);
+\`\`\`
+Pretty neat, eh?
+## Tables?
+| Feature   | Support |
+| --------- | ------- |
+| tables    | ✔ |
+| alignment | ✔ |
+| wewt      | ✔ |
+## More info?
+Read usage information and more on [GitHub](//github.com/rexxars/react-markdown)
+---------------
+A component by [Espen Hovlandsdal](https://espen.codes/)
+`
+
+class CodeBlock extends Component {
+  constructor(props) {
+    super(props)
+    this.setRef = this.setRef.bind(this)
+  }
+
+  setRef(el) {
+    this.codeEl = el
+  }
+
+  componentDidMount() {
+    this.highlightCode()
+  }
+
+  componentDidUpdate() {
+    this.highlightCode()
+  }
+
+  highlightCode() {
+    hljs.highlightBlock(this.codeEl)
+  }
+
   render() {
     return (
-       <Row style={{ backgroundColor: 'white', height: '100%' }}>
-        <Col span={12}>
-          <TableReadmeEditor />
-        </Col>
-        <Col span={12} style={{ borderLeft: '3px solid #eee' }}>
-          <TableReadmeEditor />
-        </Col>
-      </Row>
+      <pre>
+        <code ref={this.setRef} className={`language-${this.props.language}`}>
+          {this.props.value}
+        </code>
+      </pre>
+    )
+  }
+}
+
+CodeBlock.defaultProps = {
+  language: ''
+}
+
+CodeBlock.propTypes = {
+  value: PropTypes.string.isRequired,
+  language: PropTypes.string
+}
+
+class DefinitionReadmeEditor extends Component {
+  constructor(props) {
+    super(props)
+
+    this.handleControlsChange = this.handleControlsChange.bind(this)
+    this.handleMarkdownChange = this.handleMarkdownChange.bind(this)
+    this.state = {
+      markdownSrc: initialSource,
+      htmlMode: 'raw'
+    }
+  }
+
+  handleMarkdownChange(evt) {
+    this.setState({markdownSrc: evt.target.value})
+  }
+
+  handleControlsChange(mode) {
+    this.setState({htmlMode: mode})
+  }
+
+  render() {
+    return (
+      <div className="demo">
+        <div className="editor-options fixed">
+          <div className="last-saved">
+            Last saved 13 seconds ago
+          </div>
+          <div className="close">
+            <Button>Close</Button>
+          </div>
+        </div>
+        <div className="editor-pane">
+          <TableReadmeEditor value={this.state.markdownSrc} onChange={this.handleMarkdownChange} />
+        </div>
+        <div className="result-pane">
+          <Markdown
+            className="result"
+            source={this.state.markdownSrc}
+            skipHtml={this.state.htmlMode === 'skip'}
+            escapeHtml={this.state.htmlMode === 'escape'}
+            renderers={{code: CodeBlock}}
+          />
+        </div>
+      </div>
     )
   }
 }
