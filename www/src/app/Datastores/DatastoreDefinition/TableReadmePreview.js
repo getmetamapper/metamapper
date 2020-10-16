6c32cc82c17ccb350df389f77712a1860953f561
Scott <scruwys@gmail.com>
2020-10-16 10:40:25 -0400

    more

---
+++ www/src/app/Datastores/DatastoreDefinition/TableReadmePreview.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react"
+import { Col, Row } from "antd"
+
+class DefinitionReadmeEditor extends Component {
+  render() {
+    return <p>Test</p>
+  }
+}
+
+export default DefinitionReadmeEditor
--- www/src/pages/DatastoreDefinition/DefinitionReadmeEditor.js
+++ www/src/pages/DatastoreDefinition/DefinitionReadmeEditor.js
@@ -1,9 +1,20 @@
 import React, { Component } from "react"
 import { Col, Row } from "antd"
+import TableReadmeEditor from "app/Datastores/DatastoreDefinition/TableReadmeEditor"
+import TableReadmePreview from "app/Datastores/DatastoreDefinition/TableReadmePreview"
 
 class DefinitionReadmeEditor extends Component {
   render() {
-    return <p>Test</p>
+    return (
+       <Row style={{ backgroundColor: 'white', height: '100%' }}>
+        <Col span={12}>
+          <TableReadmeEditor />
+        </Col>
+        <Col span={12} style={{ borderLeft: '3px solid #eee' }}>
+          <TableReadmeEditor />
+        </Col>
+      </Row>
+    )
   }
 }
 
